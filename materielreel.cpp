6e762dcaefc7205f9ce8c58868fd0a0dc26b3244
SAMAHRI MOHAMED JIBRIL <sama0019@028F2-0G0009015.local.iut-troyes.univ-reims.fr>
2025-06-11 12:56:04 +0200

    test tfmini

--- materielreel.cpp
+++ materielreel.cpp
@@ -1,90 +1,110 @@
+// MaterielReel.cpp
+
 #include "materielreel.h"
 #include <QtMath>
+
 ILidarDriver* drv = nullptr;
+
 #ifndef _countof
 #define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
 #endif
 
-MaterielReel::MaterielReel() {
-
+MaterielReel::MaterielReel()
+{
+    // Timer LIDAR
     tictocLidar.setInterval(50);
     tictocLidar.start();
-    connect(&tictocLidar,&QTimer::timeout,
-            this,&MaterielReel::lidarValue);
+    connect(&tictocLidar, &QTimer::timeout,
+            this, &MaterielReel::lidarValue);
+
+    // Initialisation du driver LIDAR
     drv = *createLidarDriver();
     if (!drv) {
-        qDebug()<<"erreur";
-    }
-    else
-    {
-        qDebug()<<"driver ok";
-    }
-
-    IChannel* _channel;
-    _channel = (*createSerialPortChannel("/dev/ttyUSB0", 115200));
-
-    sl_result     op_result;
-    sl_lidar_response_device_info_t devinfo;
-    bool connectSuccess = false;
-    if (SL_IS_OK((drv)->connect(_channel))) {
-        op_result = drv->getDeviceInfo(devinfo);
-        if (SL_IS_OK(op_result))
-        {
-            connectSuccess = true;
-            qDebug()<<"connecté sur le lidar";
+        qDebug() << "Erreur d'initialisation du driver LIDAR";
+    } else {
+        qDebug() << "Driver LIDAR OK";
+        IChannel* channel = (*createSerialPortChannel("/dev/ttyUSB0", 115200));
+        if (SL_IS_OK(drv->connect(channel))) {
+            sl_lidar_response_device_info_t devinfo;
+            if (SL_IS_OK(drv->getDeviceInfo(devinfo))) {
+                qDebug() << "Connecté sur le LIDAR";
+                    qDebug() << "SN:" << devinfo.serialnum;
+                qDebug() << "Firmware:" << (devinfo.firmware_version >> 8) << "." << (devinfo.firmware_version & 0xFF)
+                         << " HW rev:" << (int)devinfo.hardware_version;
+            } else {
+                delete drv;
+                drv = nullptr;
+                qDebug() << "Impossible de récupérer les infos du LIDAR";
+            }
+            drv->setMotorSpeed();
+            drv->startScan(0, 1);
+        } else {
+            qDebug() << "Échec de connexion au LIDAR";
         }
-        else{
-            delete drv;
-            drv = NULL;
-        }
-        qDebug()<<"SLAMTEC LIDAR S/N: "<<devinfo.serialnum;
-        qDebug()<<"Firmware Ver: "<<(devinfo.firmware_version>>8)<<"."<<(devinfo.firmware_version & 0xFF)
-                 <<"Hardware Rev: "<<(int)devinfo.hardware_version;
     }
-    drv->setMotorSpeed();
-    drv->startScan(0,1);
 
+    // Initialisation du capteur TFmini
+    tfmini = new TFmini();
+    if (!tfmini->init()) {
+        qDebug() << "Erreur d'initialisation du TFmini";
+    } else {
+        qDebug() << "TFmini initialisé avec succès";
+    }
+
+    // Timer TFmini
+    tictocTFmini.setInterval(100);
+    tictocTFmini.start();
+    connect(&tictocTFmini, &QTimer::timeout,
+            this, &MaterielReel::tfminiValue);
 }
-
 
 void MaterielReel::lidarValue()
 {
     sl_lidar_response_measurement_node_hq_t nodes[8192];
-    size_t   count = _countof(nodes);
+    size_t count = _countof(nodes);
     sl_result op_result = drv->grabScanDataHq(nodes, count);
 
-    if (SL_IS_OK(op_result))
-    {
+    if (SL_IS_OK(op_result)) {
         distances_mm.fill(0);
-        for (int i=0;i<(int)count;i++)
-        {
-            int angle=round((nodes[i].angle_z_q14 * 90.f) / 16384.f);
-            if (angle>=180) angle=angle-360;
-            if ((angle>=-180)and(angle<180))
-            {
-                int d=nodes[i].dist_mm_q2/4.0;
-                if (d>0) distances_mm.at(angle+180)=d;
+        for (int i = 0; i < int(count); ++i) {
+            int angle = qRound((nodes[i].angle_z_q14 * 90.0f) / 16384.0f);
+            if (angle >= 180) angle -= 360;
+            if (angle >= -180 && angle < 180) {
+                int d = nodes[i].dist_mm_q2 / 4.0;
+                if (d > 0) distances_mm.at(angle + 180) = d;
             }
         }
-        for (int i=0;i<360;i++)
-        {
-            if (distances_mm.at(i)==0)
-            {
-                for (int j=1;j<10;j++)
-                {
-                    int pos=(i+j)%360;
-                    if (distances_mm.at(pos)!=0) distances_mm.at(i)=distances_mm.at(pos);
+        // comble les trous
+        for (int i = 0; i < 360; ++i) {
+            if (distances_mm.at(i) == 0) {
+                for (int j = 1; j < 10; ++j) {
+                    int pos = (i + j) % 360;
+                    if (distances_mm.at(pos) != 0) {
+                        distances_mm[i] = distances_mm[pos];
+                        break;
+                    }
                 }
             }
         }
+        emit newDatas();
+    } else {
+        qDebug() << "Erreur de lecture LIDAR";
     }
-    emit newDatas();
+}
 
+void MaterielReel::tfminiValue()
+{
+    int distance = tfmini->getDistance();
+    if (distance > 0) {
+        qDebug() << "TFmini Distance:" << distance << "cm";
+        tfminiDistance = distance;
+    } else {
+        qDebug() << "Erreur de lecture TFmini";
+    }
 }
 
 void MaterielReel::deplacer(double _vitesse, double _angle)
 {
     vitesse.setPosition(_vitesse);
     direction.setPosition(_angle);
-
 }
--- materielreel.h
+++ materielreel.h
@@ -1,14 +1,16 @@
+// MaterielReel.h
 #ifndef MATERIELREEL_H
 #define MATERIELREEL_H
 
+#include <QObject>
+#include <QTimer>
+#include <QDebug>
 #include "pwm.h"
 #include "servomoteur.h"
 #include "materiel.h"
-#include <QObject>
-#include <QDebug>
-#include <QTimer>
 #include "tfmini.h"
 #include "sl_lidar_driver.h"
+
 using namespace std;
 using namespace sl;
 
@@ -16,16 +18,26 @@ extern ILidarDriver* drv;
 
 class MaterielReel : public Materiel
 {
+    Q_OBJECT
+
 public:
     MaterielReel();
+
 public slots:
+    void tfminiValue();
     void lidarValue();
     void deplacer(double _vitesse, double _angle);
+
 signals:
+    void newDatas();
+
 private:
     QTimer tictocLidar;
     ServoMoteur vitesse{2,1500,300,300};
     ServoMoteur direction{3,1500,300,300};
+    TFmini *tfmini;              // Capteur TFmini
+    QTimer tictocTFmini;
+    int tfminiDistance{0};
 };
 
 #endif // MATERIELREEL_H
